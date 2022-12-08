5c3cea556433432b06275735bf20a8db81cfd0fe
JunKyu Lee <wnsrb0407@mju.ac.kr>
2022-12-08 13:38:43 +0900

    6th commit
    hi
    됐다!!
     // asdasd

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include <list>
 #include <string>
 
+// 머야 왜 안돼
+// 머야 왜 안돼2
 using namespace std;
 
 int main() {
 	// hihihihihihihihi
 	vector<int> v;
+	list<int> l;
 	cout << "Hello world" << endl;
 }
 // git add -p 확인용
